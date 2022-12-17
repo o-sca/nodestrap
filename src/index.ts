ac1b78cc3cf6c6dbc3af64f250cbd0bd95950ccb
oscar <97479266+o-sca@users.noreply.github.com>
2022-12-16 16:25:28 -0800

    feat: add version checker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,79 @@
 #!/usr/bin/env node
 import nodestrap from "./nodestrap";
+import { version } from "../package.json";
+import https from "node:https";
+import { IncomingMessage } from "node:http";
 
-nodestrap();
+type NpmBody = {
+  "dist-tags": {
+    latest: string;
+  };
+}
+
+/**
+* Compare two semver versions. Returns true if version A is greater than
+* version B
+* @param {string} versionA
+* @param {string} versionB
+* @returns {boolean}
+*/
+function compareSemVer(versionA: string, versionB: string): boolean {
+  const versionsA = versionA.split(/\./g);
+  const versionsB = versionB.split(/\./g);
+
+  while (versionsA.length || versionsB.length) {
+    const a = Number(versionsA.shift());
+    const b = Number(versionsB.shift());
+
+    if (a == b)
+      continue;
+
+    return (a > b || isNaN(b));
+  }
+  return false;
+}
+
+
+/**
+ * Pull latest version value on npmjs.
+ * @returns {Promise<string>} npmjs' latest version
+ */
+function pullNpmJsVersion(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    https.get("https://registry.npmjs.org/@o-sca/nodestrap", (res: IncomingMessage) => {
+      let body = '';
+
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      res.on('end', () => {
+        const jsonBody = (JSON.parse(body) as NpmBody);
+        resolve(jsonBody["dist-tags"].latest)
+      })
+    }).on('error', (e) => {
+      reject(e);
+    });
+  })
+}
+
+/**
+ * Compares both version and prompts user to update if new version
+ * is available.
+ */
+function checkAndRun(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    pullNpmJsVersion()
+      .then((npmJsVer: string) => {
+        const currVer = version as string;
+        if (compareSemVer(npmJsVer, currVer)) {
+          console.log(`New update for nodestrap!\nLatest Version: ${npmJsVer}\nCurrent Version: ${currVer}\n`)
+          console.log("Run `npm i -g @o-sca/nodestrap` to pull the latest updates.\n");
+        }
+        resolve();
+      })
+      .catch(reject)
+  })
+}
+
+checkAndRun().catch(console.error).finally(() => nodestrap());
