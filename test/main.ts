281f2e2a58e614b5ecae53e0622613b242c25196
oscar <97479266+o-sca@users.noreply.github.com>
2022-12-24 23:59:49 +0900

    refactor(test): change file name

--- test/main.ts
+++ test/main.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { mkdirSync } from "node:fs";
+import { mkdirSync, existsSync } from "node:fs";
 import path from "node:path";
 import shelljs from "shelljs";
 import nodestrap from "../src/nodestrap";
@@ -46,4 +46,35 @@ describe('Main Nodestrap Tests', () => {
       })
   });
 
+  it('create ts project', () => {
+    process.argv = [
+      'node', 'dist/src/index.js',
+      '-p', 'ts',
+      '-n', 'ts-test',
+      '-a', 'real dev',
+      '-i', 'npm',
+      '-g', 'false'
+    ];
+
+    function checkTargetPath(): boolean {
+      const exist = existsSync(filledOptions.targetPath);
+      if (exist) {
+        shelljs.cd(process.cwd());
+        shelljs.exec(`rm -r ${filledOptions.projectName}`);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    const parsedArgs = parseArgs(process.argv);
+    const filledOptions = initOptions(options, parsedArgs);
+    const questionsLeft = checkMissingPrompts(filledOptions)
+
+    nodestrap(filledOptions, questionsLeft).then((result) => {
+      if (result) {
+        expect(checkTargetPath()).to.equal(true);
+      }
+    }).catch(console.error)
+  });
 });
