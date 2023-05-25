8d961f48356b09e2b1a9634adb4ea289d3f2362c
Oscar Zhu <ozhu35@gmail.com>
2023-05-25 14:16:50 -0700

    feat: add handler

---
+++ src/handler/handler.ts
@@ -0,0 +1,28 @@
+import { select, confirm, input } from '@inquirer/prompts';
+
+interface Handler {
+  setNext(h: Handler): Handler;
+  handle(request: {
+    [key: string]: string | boolean;
+  }): { [key: string]: string | boolean } | null;
+}
+
+export type Prompts = typeof select | typeof confirm | typeof input;
+
+export class BaseHandler implements Handler {
+  private _next: Handler;
+
+  public setNext(handler: Handler): Handler {
+    this._next = handler;
+    return handler;
+  }
+
+  public handle(request: {
+    [key: string]: string | boolean;
+  }): { [key: string]: string | boolean } | null {
+    if (this._next) {
+      return this._next.handle(request);
+    }
+    return null;
+  }
+}
