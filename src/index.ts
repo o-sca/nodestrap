0580fb418b78a16e7db9c4be9060e17e5d9ea04c
Oscar Zhu <ozhu35@gmail.com>
2023-05-25 21:10:56 -0700

    refactor: update entry

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
 #!/usr/bin/env node
-import { Lang, Manager, Options } from "./types"
-import { checkAndRun } from "./checkSemVer";
-import { checkMissingPrompts, initOptions, parseArgs } from "./cli";
-import nodestrap from "./nodestrap";
+import { Nodestrap } from './nodestrap';
 
-const options: Options = {
-  projectName: "",
-  authorName: "",
-  templateChoice: Lang.default,
-  templatePath: "",
-  targetPath: "",
-  gitInit: false,
-  packageManager: Manager.default,
-  skipPrompt: false
-}
-
-checkAndRun()
-  .catch(console.error)
-  .finally(() => {
-    const parsedArgs = parseArgs(process.argv);
-    const filledOptions = initOptions(options, parsedArgs);
-    const questionsLeft = checkMissingPrompts(filledOptions);
-    nodestrap(filledOptions, questionsLeft).catch(console.error);
-  });
+const nodestrap = new Nodestrap();
+nodestrap.launch();
--- src/nodestrap.ts
+++ src/nodestrap.ts
@@ -1,67 +1,75 @@
-import chalk from "chalk";
-import path from "node:path";
-import inquirer, { PromptModule, Question } from "inquirer";
-import { NSAnswers, Options } from "./types";
-import { installDeps, execGitInit, createProject, createDirContents } from "./functions";
+import { select, confirm, input } from '@inquirer/prompts';
+import {
+  DependencyHandler,
+  GitHandler,
+  ProjectHandler,
+  TemplateHandler,
+} from './handler';
 
-const CURR_DIR = process.cwd();
+export class Nodestrap {
+  async launch() {
+    const options = await this.prompt();
 
-/**
-* Prints the success message.
-* @param {Options} options
-*/
-function successLog(options: Options): boolean {
-  const optionsCopy = {
-    projectName: options.projectName,
-    authorName: options.authorName,
-    templateChoice: options.templateChoice,
-    targetPath: options.targetPath,
-    packageManager: options.packageManager,
-    gitInit: options.gitInit
-  };
-  console.log(chalk.greenBright(`⚡Project successfully generated!\n`));
-  console.dir(optionsCopy);
-  return true;
+    const projectHandler = new ProjectHandler();
+    const templateHandler = new TemplateHandler();
+    const dependencyHandler = new DependencyHandler();
+    const gitHandler = new GitHandler();
+
+    templateHandler
+      .setNext(projectHandler)
+      .setNext(dependencyHandler)
+      .setNext(gitHandler);
+
+    const result = templateHandler.handle(options);
+    console.log(result);
+  }
+
+  private async prompt() {
+    const options = {
+      template: await select<string>({
+        message: 'select project environment',
+        choices: [
+          {
+            name: 'typescript',
+            value: 'typescript',
+          },
+          {
+            name: 'javascript',
+            value: 'javascript',
+          },
+        ],
+      }),
+      project: await input({
+        message: 'input project name',
+        validate: (input: string) => {
+          //eslint-disable-next-line
+          if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+          else
+            return 'project name may only include letters, numbers, underscores and hashes.';
+        },
+      }),
+      author: await input({
+        message: 'input author name',
+      }),
+      git: await confirm({ message: 'initialise git?' }),
+      packageManager: await select<string>({
+        message: 'select package manager',
+        choices: [
+          {
+            name: 'npm',
+            value: 'npm',
+          },
+          {
+            name: 'pnpm',
+            value: 'pnpm',
+          },
+          {
+            name: 'yarn',
+            value: 'yarn',
+          },
+        ],
+      }),
+    };
+    return options;
+  }
 }
-
-function errorLog(error: string): boolean {
-  console.error(chalk.redBright(error));
-  return false;
-}
-
-/**
- * Main driver.
- */
-async function nodestrap(options: Options, questions: Question<NSAnswers>[]): Promise<boolean> {
-  const prompt: PromptModule = inquirer.createPromptModule();
-  const answers = await prompt(questions);
-
-  const templateChoice = answers["templateChoice"] || options.templateChoice;
-  const projectName = answers["projectName"] || options.projectName;
-  const authorName = answers["authorName"] || options.authorName;
-  const templatePath = path.join(__dirname, '..', 'templates', templateChoice);
-  const targetPath = path.join(CURR_DIR, projectName);
-  const gitInit = answers["gitInit"] || options.gitInit;
-  const packageManager = answers.packageManager || options.packageManager;
-
-  const finalOptions: Options = {
-    skipPrompt: options.skipPrompt,
-    gitInit: gitInit,
-    projectName: projectName,
-    authorName: authorName,
-    templateChoice: templateChoice,
-    templatePath: templatePath,
-    targetPath: targetPath,
-    packageManager: packageManager
-  };
-
-  console.log(chalk.yellowBright(`Generating project...`));
-
-  if (!createProject(targetPath)) return errorLog("Failed to create new project");
-  createDirContents(templatePath, projectName, authorName, CURR_DIR);
-  if (!installDeps(finalOptions)) return errorLog("Failed to install dependencies");
-  if (!execGitInit(finalOptions)) return errorLog("Failed to execute `git init`");
-  return successLog(finalOptions);
-}
-
-export default nodestrap;
