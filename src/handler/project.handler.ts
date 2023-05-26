158c4d567a1105b1a43a3ee5b0c9362ab3418efb
Oscar Zhu <ozhu35@gmail.com>
2023-05-25 21:06:24 -0700

    feat: add project handler

---
+++ src/handler/project.handler.ts
@@ -0,0 +1,26 @@
+import path from 'node:path';
+import chalk from 'chalk';
+import { BaseHandler } from './handler';
+import { Craft } from '../functions/craft';
+
+export class ProjectHandler extends BaseHandler {
+  private readonly CURR_DIR = process.cwd();
+  public handle(request: { [key: string]: string | boolean }) {
+    if (request['project'] !== undefined) {
+      const authorName = request['author'] as string;
+      const templatePath = request['template'] as string;
+      const projectName = request['project'] as string;
+      const newProjectPath = path.join(this.CURR_DIR, projectName);
+
+      request['project'] = newProjectPath;
+
+      console.log(chalk.yellowBright(`Generating project...`));
+      const createdProjectPath = Craft.project(newProjectPath);
+      if (!createdProjectPath) throw new Error('Failed to create new project');
+      Craft.dir(templatePath, projectName, authorName, this.CURR_DIR);
+
+      return super.handle(request);
+    }
+    return request;
+  }
+}
