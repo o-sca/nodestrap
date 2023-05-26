19ead4eec636760fd46823c184ef16055a801977
Oscar Zhu <ozhu35@gmail.com>
2023-05-25 21:06:39 -0700

    refactor: change to template handler

---
+++ src/handler/template.handler.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import { BaseHandler } from './handler';
+
+export class TemplateHandler extends BaseHandler {
+  public handle(request: {
+    [key: string]: string | boolean;
+  }): { [key: string]: string | boolean } | null {
+    if (request['template'] !== undefined) {
+      const template = request['template'] as string;
+      request['template'] = path.join(
+        __dirname,
+        '../..',
+        'templates',
+        template
+      );
+      return super.handle(request);
+    }
+    return request;
+  }
+}
