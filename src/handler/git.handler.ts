d19f66bb14f46092c34cba5aba9d100970e826d5
oscar <97479266+o-sca@users.noreply.github.com>
2023-09-24 12:00:59 -0700

    fix(template): missing dir name to rm command

--- src/handler/git.handler.ts
+++ src/handler/git.handler.ts
@@ -1,11 +1,7 @@
 import fs from 'node:fs/promises';
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import shell from 'shelljs';
 import { BaseHandler } from './handler.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export class GitHandler extends BaseHandler {
   public async handle(request: { [key: string]: boolean | string }) {
@@ -21,7 +17,7 @@ export class GitHandler extends BaseHandler {
 
   private async createGitignore(projectPath: string) {
     const gitignoreFile = await fs.readFile(
-      path.join(__dirname, '..', '..', 'templates', 'typescript', '.gitignore'),
+      path.join(__dirname, 'templates', 'typescript', '.gitignore'),
       'utf-8'
     );
     await fs.writeFile(`${projectPath}/.gitignore`, gitignoreFile);
--- src/handler/template.handler.ts
+++ src/handler/template.handler.ts
@@ -1,20 +1,11 @@
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import { BaseHandler } from './handler.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export class TemplateHandler extends BaseHandler {
   public async handle(request: { [key: string]: string | boolean }) {
     if (request['template'] !== undefined) {
       const template = request['template'] as string;
-      request['template'] = path.join(
-        __dirname,
-        '../..',
-        'templates',
-        template
-      );
+      request['template'] = path.join(__dirname, 'templates', template);
       return await super.handle(request);
     }
     return request;
