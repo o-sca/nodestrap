2779480d5be04ff54d30ddda87adb88368f4ab8e
Oscar Zhu <ozhu35@gmail.com>
2024-01-02 10:31:31 -0800

    chore: simple import checker

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  root: true,
-  env: {
-    node: true,
-  },
-  ignorePatterns: ['templates/**/*', 'tests', '.eslintrc.*'],
-  extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
-  overrides: [],
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  rules: {},
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    root: true,
+    env: {
+        node: true,
+    },
+    ignorePatterns: ['templates/**/*', 'tests', '.eslintrc.*'],
+    extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
+    overrides: [],
+    plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
 };
