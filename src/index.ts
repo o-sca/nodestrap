fc5757926914f2a58ba9068b140c442d0bf1d150
oscar <97479266+o-sca@users.noreply.github.com>
2024-05-21 16:03:10 -0700

    ci: npm token

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import { Nodestrap } from './nodestrap.js';
+import { Updater } from './updater.js';
 
 const nodestrap = new Nodestrap();
+const updater = new Updater();
 
 (async () => {
 	await updater.fetch();
