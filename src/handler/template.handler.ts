ac87631ea55e2f272327620b7606742dedef5f66
oscar <97479266+o-sca@users.noreply.github.com>
2024-05-21 17:48:37 -0700

    chore: formatting

--- src/handler/template.handler.ts
+++ src/handler/template.handler.ts
@@ -3,10 +3,6 @@ import { cwd } from 'node:process';
 
 import { BaseHandler } from './handler.js';
 
-const __fileURL = fileURLToPath(import.meta.url);
-const __filePath = path.parse(__fileURL);
-const __dirname = path.dirname(__filePath.dir);
-
 export class TemplateHandler extends BaseHandler {
 	public async handle(request: { [key: string]: string | boolean }) {
 		if (request['template'] !== undefined) {
